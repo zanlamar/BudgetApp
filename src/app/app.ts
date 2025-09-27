bb66e73e41b7cf0e3e0fa7fedf2a85f0d604c113
Ana Laf <zanlamar@gmail.com>
2025-09-27 18:42:10 +0200

    Home component implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+  standalone: true
+})
+export class Home {
+
+}
