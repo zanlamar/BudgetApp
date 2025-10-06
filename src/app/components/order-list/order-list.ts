20e405549df41937e273a12e9b71c55cf68ccc22
Ana Laf <zanlamar@gmail.com>
2025-10-06 20:09:56 +0200

    signal for service filter done

--- src/app/components/order-list/order-list.ts
+++ src/app/components/order-list/order-list.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal, computed } from '@angular/core';
+import { Component, Input, signal, computed, effect } from '@angular/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FinalCard } from '../final-card/final-card';
 import { SubmissionData } from '../../../types/types';
@@ -12,25 +12,41 @@ import { SubmissionData } from '../../../types/types';
 })
 export class OrderList {
 
- @Input() orders: SubmissionData[] = [];
+  ordersSignal = signal<SubmissionData[]>([]);
+  private _orders: SubmissionData[] = [];
+
+  @Input()
+  get orders(): SubmissionData[] {
+    return this._orders;
+  }
+  set orders(value: SubmissionData[]) {
+    this._orders = value ?? [];
+    // Actualizamos el signal con una **nueva referencia** para asegurar reactividad
+     this.ordersSignal.set([...this._orders]);
+  }
+
   selectedFilter = signal<string>('all');
 
-  filteredOrders = computed(() => {
-    const filter = this.selectedFilter();
-    return this.orders.filter(order => {
-      const services = order.services ?? {};
-
-      switch (filter) {
-        case 'web':
-          return !!services.web;
-        case 'seo':
-          return !!services.seo;
-        case 'ads':
-          return !!services.ads;
-        case 'all':
-        default:
-          return services.web || services.seo || services.ads;
+  constructor() {
+    effect(() => {
+      const currentOrders = this.ordersSignal();
+      if (currentOrders.length) {
+        this.selectedFilter.set('all');
       }
     });
+  }
+
+ filteredOrders = computed(() => {
+  const filter = this.selectedFilter();
+  return this.ordersSignal().filter(order => {
+    switch (filter) {
+      case 'web': return !!order.services?.web;
+      case 'seo': return !!order.services?.seo;
+      case 'ads': return !!order.services?.ads;
+      case 'all':
+      default: 
+        return !!order.services?.web || !!order.services?.seo || !!order.services?.ads;
+    }
   });
-}
+});
+}  
